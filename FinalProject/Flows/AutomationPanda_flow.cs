85c5c64731196f9fa10fff08290be4f46c185289
Shay Mizrahi <shaymizrahi2@gmail.com>
2022-02-03 15:27:55 +0200

    roll back

--- FinalProject/Flows/AutomationPanda_flow.cs
+++ FinalProject/Flows/AutomationPanda_flow.cs
@@ -10,17 +10,17 @@ namespace FinalProject.Flows
     {
         public BasicActions actions;
         public IWebDriver driver;
-        public AutomationPanda autoPanda2;
-        public AutomationPanda_flow(BasicActions actions, IWebDriver driver, AutomationPanda autoPanda2)
+        public AutomationPanda autoPanda;
+        public AutomationPanda_flow(BasicActions actions, IWebDriver driver, AutomationPanda autoPanda)
         {
             this.actions = actions;
             this.driver = driver;
-            this.autoPanda2 = autoPanda2;
+            this.autoPanda = autoPanda;
         }
         public void OpenSite(string websiteName)
         {
             IReportMng.IReporter.CreatNode("Open webSite " + websiteName);
-            IWebElement site = actions.SearchElement(autoPanda2.DemoSiteList, websiteName, "web sites");
+            IWebElement site = actions.SearchElement(autoPanda.DemoSiteList, websiteName, "web sites");
             actions.ScrollToView(site, websiteName);
             actions.ClickOnElement(site, websiteName);
         }
